1390b9b8b8c68786636e7de756266347468dd7cc
unknown <arsenypie@mail.ru>
2023-10-01 19:45:42 +0800

    add file

---
+++ easytask_S31.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string a[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+	int x;
+	cin>>x;
+	cout<<a[x];
+	return 0;
+}
