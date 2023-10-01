f0f4c44ce81cc5f689806d4773740ea77154feb7
unknown <arsenypie@mail.ru>
2023-10-01 15:14:22 +0800

    add file

---
+++ easytask_S21.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+    cout << "Enter variables A and B: ";
+    cin >> A >> B;
+        for(int i = A; i<=B; i++)
+            {
+            cout << i * 3 << "\n";
+            }
+}
